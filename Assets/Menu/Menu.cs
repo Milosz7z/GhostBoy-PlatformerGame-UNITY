6051eab9bc478806d0c5d6fa46ed3d704cfd10b2
Miłosz <milosz.synecki@gmail.com>
2017-01-29 21:22:33 +0100

    Dużo dodatków

---
+++ Assets/Menu/Menu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public Canvas MainCanvas;
+    public Canvas OptionCanvas;
+	public Canvas LevelCanvas;
+
+    void Awake()
+    {
+        OptionCanvas.enabled = false;
+		LevelCanvas.enabled = false;
+    }
+
+    public void OptionsOn()
+    {
+        OptionCanvas.enabled = true;
+        MainCanvas.enabled = false;
+    }
+
+	public void LevelsOn()
+	{
+		LevelCanvas.enabled = true;
+		MainCanvas.enabled = false;
+	}
+
+    public void ReturnMain()
+    {
+        OptionCanvas.enabled = false;
+		LevelCanvas.enabled = false;
+        MainCanvas.enabled = true;
+    }
+}
